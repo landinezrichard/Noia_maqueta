9bd7a1ff305ea510447412e80f1fa83d44ea6f6a
Ricardo <landinezrichard@gmail.com>
2017-05-20 12:28:34 -0500

    Listo el home / botones 'Ir arriba' y 'ir seccion siguiente banner principal'

--- dev/js/functions.js
+++ dev/js/functions.js
@@ -4,4 +4,30 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('.Menu').slideToggle();
 	});
+
+	/*Boton Ir arriba*/
+	$('.ButtonIrUp').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ButtonIrUp').slideDown(300);
+		} else {
+			$('.ButtonIrUp').slideUp(300);
+		}
+	});
+
+	/*SCRIPTS SOLO HOME*/
+
+	/*Botton home, seccion siguiente*/
+
+	$('.Banner-btnDown').click(function(){
+		var target = $('.HomeButtons');
+		$('body, html').animate({
+			scrollTop: $(target).offset().top
+		}, 300);
+	});
 });
